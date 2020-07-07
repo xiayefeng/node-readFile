14698eede8dbd575c18c5221ad46dd7a09f2354c
xiayefeng <xiayu_12@yeah.net>
2020-07-07 18:19:01 +0800

    add file

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const fs = require('fs')
 
-fs.readFile('./file/20200706.log', 'utf8', (err, data) => {
+fs.readFile('./static/20200706.log', 'utf8', (err, data) => {
   if(err) throw err
   // console.log(data)
   let arr = data.split(/\n/)
