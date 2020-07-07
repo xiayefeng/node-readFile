38806e4f0e96e7d742c360dd2776e2222d546c50
xiayefeng <xiayu_12@yeah.net>
2020-07-07 23:08:13 +0800

    add output file

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const fs = require('fs')
+const path = require('path')
 
 fs.readFile('./static/20200706.log', 'utf8', (err, data) => {
   if(err) throw err
@@ -17,16 +18,29 @@ fs.readFile('./static/20200706.log', 'utf8', (err, data) => {
    
     return {
       ip: arr[0],
-      date: new Date(date),
+      date: getDate(new Date(date)),
       method: arr3[0].slice(1),
       path: arr3[1],
       protocol: arr3[2] && arr3[2].slice(0, -1) || '',
       status: +arr4[0],
       size: +arr4[1],
+      currentUrl: arr4[2].length > 2 ? arr4[2].slice(1, -1) : arr4[2],
       ua: arr2[2].slice(1, -1)
     }
   })
-  console.log(arr2.length)
+  // console.log(arr2.length)
   let arr3 = arr2.filter(item => ['POST', 'GET'].includes(item.method))
-  console.log(arr3.length)
+  let arr4 = arr3.filter(item => item.currentUrl && item.currentUrl !== '-')
+  // console.log(arr4.length)
+  const filePath = path.join(__dirname, './static/test.json')
+  fs.writeFile(filePath, JSON.stringify(arr4, null, 2), {encoding: 'utf-8', flag: 'w'}, (err) => {
+    if(err) throw err
+  })
 })
+
+function getDate (date) {
+  let hour = date.getHours()
+  hour += 8
+  date.setHours(hour)
+  return date
+}
